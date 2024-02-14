803d43d0a6067b6ea8b77b28b0bab2fd48157be8
shivam096 <shivam96@uw.edu>
2024-02-14 01:46:30 -0800

    Add process function

---
+++ backend/processing.py
@@ -0,0 +1,6 @@
+from pprint import pprint
+from req import get_news_articles
+from transform import get_filter_dates
+
+
+get_filter_dates("/Users/stlp/Documents/data557/final_project/dinero/data/AAPL.csv",5, "AAPL")
