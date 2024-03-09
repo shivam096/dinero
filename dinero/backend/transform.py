b7104d96912d4066f4f678950f1900b8296ad23d
shivam096 <shivam96@uw.edu>
2024-03-09 15:28:47 -0800

    Update filter condition

--- dinero/backend/transform.py
+++ dinero/backend/transform.py
@@ -25,9 +25,9 @@ def find_count_value_change(file : str, value_change: int) -> list:
     stock_data['Percent Change'] = (stock_data['Value Change']/stock_data['Open'])*100
 
     if value_change >= 0:
-        change_df = stock_data[stock_data['Percent Change']>value_change]
+        change_df = stock_data[stock_data['Percent Change']>=value_change]
     else:
-        change_df = stock_data[stock_data['Percent Change']<value_change]
+        change_df = stock_data[stock_data['Percent Change']<=value_change]
 
     return change_df['Date'].to_list()
 
