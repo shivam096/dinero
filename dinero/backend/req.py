34be0b145f5ec982d7729162af3525a41c1f2262
shivam096 <shivam96@uw.edu>
2024-03-09 14:38:41 -0800

    Add dockstring

--- dinero/backend/req.py
+++ dinero/backend/req.py
@@ -1,6 +1,25 @@
+"""
+Module for retrieving news articles related to a specific stock ticker.
+
+    Function :
+    -   get_news_articles
+
+"""
 import requests
 
 def get_news_articles(ticker : str, date: str) :
+    """
+    Retrieve news articles related to a specific stock ticker on a given date.
+
+    Args:
+        ticker (str): The stock ticker symbol.
+        date (str): The date for which news articles are to be retrieved.
+                    Should be in the format 'YYYY-MM-DD'.
+    Returns:
+        dict: A dictionary containing the news articles data.
+
+    
+    """
     url = f'https://eodhd.com/api/news?s={ticker}&offset=0&api_token=demo&fmt=json&from={date}&to={date}'
-    data = requests.get(url).json()
+    data = requests.get(url, timeout= 100).json()
     return data
