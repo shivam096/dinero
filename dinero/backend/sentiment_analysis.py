b07ea6140dbb0060fb58925b9a3c9b654aed0cd0
shivam096 <shivam96@uw.edu>
2024-03-13 12:54:37 -0700

    Add error handling

--- dinero/backend/sentiment_analysis.py
+++ dinero/backend/sentiment_analysis.py
@@ -16,7 +16,8 @@ def get_sentiment_value(title_list: list) -> dict:
     VADER (Valence Aware Dictionary and sEntiment Reasoner).
 
     Args:
-    - title_list (list): A list of strings representing titles or sentences to analyze.
+    - title_list (list): A list of dictionaries representing titles or sentences to analyze.
+                         Each dictionary should have 'title', 'content', and 'link' keys.
 
     Returns:
     - dict: A dictionary where keys are the titles/sentences and 
@@ -27,7 +28,15 @@ def get_sentiment_value(title_list: list) -> dict:
     senti_dict = {}
     analyzer = SentimentIntensityAnalyzer()
     for sentence in title_list:
-        vs = analyzer.polarity_scores(sentence.get('content'))
-        senti_dict[sentence.get('title')] = {'sentiment_score':vs, 'link':sentence.get('link')}
+        try:
+            content = sentence['content']
+            title = sentence['title']
+            link = sentence['link']
+            vs = analyzer.polarity_scores(content)
+            senti_dict[title] = {'sentiment_score': vs, 'link': link}
+        except KeyError as e:
+            print(f"Error: Missing key in input data - {e}")
+        except Exception as e:
+            print(f"Error occurred while processing: {e}")
 
     return senti_dict
